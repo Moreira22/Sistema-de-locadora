13d4e838db776689f8577c0af8f9104abc76822a
joao.paula <joao.paula@basisti.com.br>
2025-09-28 23:16:48 -0300

    fix: add service

--- src/main/java/com/dwws/locadora/service/UsuarioService.java
+++ src/main/java/com/dwws/locadora/service/UsuarioService.java
@@ -1,11 +1,16 @@
 package com.dwws.locadora.service;
 
+import com.dwws.locadora.domain.Funcionario;
 import com.dwws.locadora.domain.Usuario;
+import com.dwws.locadora.repository.FuncionarioRepository;
 import com.dwws.locadora.repository.UsuarioRepository;
+import com.dwws.locadora.service.dto.FuncionarioDTO;
+import com.dwws.locadora.service.dto.FuncionarioProjection;
 import com.dwws.locadora.service.dto.UserPasswordChangeDTO;
 import com.dwws.locadora.service.dto.UsuarioDTO;
 import com.dwws.locadora.service.dto.UsuarioListDTO;
 import com.dwws.locadora.service.exception.EntityNotFoundException;
+import com.dwws.locadora.service.mapper.FuncionarioMapper;
 import com.dwws.locadora.service.mapper.UsuarioMapper;
 import com.dwws.locadora.service.util.MensagemUsuarioUtil;
 import lombok.RequiredArgsConstructor;
@@ -22,9 +27,15 @@ public class UsuarioService {
     private final UsuarioRepository repository;
     private final UsuarioMapper mapper;
     private final PasswordEncoder passwordEncoder;
+    private final FuncionarioMapper  funcionarioMapper;
+    private final FuncionarioRepository  funcionarioRepository;
 
     public Usuario findEntity(Long id) {
         return repository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException(MensagemUsuarioUtil.ENTITY_NOT_FOUND));
+    }
+    public Funcionario findFuncionario(Long id) {
+        return funcionarioRepository.findById(id).orElseThrow(
                 () -> new EntityNotFoundException(MensagemUsuarioUtil.ENTITY_NOT_FOUND));
     }
 
@@ -62,4 +73,18 @@ public class UsuarioService {
                 .map(mapper::toDto)
                 .orElseThrow( () -> new EntityNotFoundException(MensagemUsuarioUtil.ENTITY_NOT_FOUND));
     }
+
+    public FuncionarioDTO saveFuncionario(FuncionarioDTO dto){
+        return funcionarioMapper.toDto(funcionarioRepository.save(funcionarioMapper.toEntity(dto)));
+    }
+
+    public FuncionarioDTO findFuncionarioByID(Long id) {
+        return funcionarioMapper.toDto(findFuncionario(id));
+    }
+
+    public Page<FuncionarioProjection> findAllFuncionario(Pageable pageable) {
+        return funcionarioRepository.listAll(pageable);
+    }
+
+
 }
